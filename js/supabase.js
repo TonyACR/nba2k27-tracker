9661549977bc98de7d98ede25b3f553741de74ec
TonyACR <agrillocristian3@gmail.com>
2026-08-09 15:49:57 +0200

    Initial commit

---
+++ js/supabase.js
@@ -0,0 +1,10 @@
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm";
+
+const SUPABASE_URL = "https://qytqvqruzsscxyatdcol.supabase.co";
+
+const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_T1f3SrRLaBADTMfFrh9PYQ_WbZcxmda";
+
+export const supabase = createClient(
+    SUPABASE_URL,
+    SUPABASE_PUBLISHABLE_KEY
+);
